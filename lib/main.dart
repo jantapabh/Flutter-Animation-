bd2a49e31bbba583791166d2401b83fa17baefcb
jantapabh <s6035512034@phuket.psu.ac.th>
2020-03-24 03:00:00 +0700

    add ShapeShiht

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,135 @@
-// Fade in and Fade out Animation
-// กดปุ่มแล้วแสดงคำอธิบายรูปภาพ
-import'package:flutter/material.dart';
+//// Fade in and Fade out Animation
+//// กดปุ่มแล้วแสดงคำอธิบายรูปภาพ
+//import'package:flutter/material.dart';
+//
+//void main() => runApp(MyApp());
+//
+//class MyApp extends StatelessWidget{
+//  @override
+//  Widget build(BuildContext context) {
+//
+//   return MaterialApp(
+//     title: "Animation",
+//     home: Scaffold(
+//       appBar: AppBar(title: Text("Animation"),),
+//       body: FadeInDemo(),
+//       //Show FadeInDemo Class
+//     ),
+//
+//   );
+//  }
+//}
+//
+//// FadeInDemo class create FadeInDemoState and extends StatefulWidget
+//class FadeInDemo extends StatefulWidget{
+//  FadeInDemoState createState() => FadeInDemoState();
+//  //FadeInDemoState createState () => FadeInDemoState();
+//}
+//
+//class FadeInDemoState extends State<FadeInDemo>{
+//  var opacity = 0.0;
+//  @override
+//
+//   //Display Main State
+//  // onPressed สำหนับกดปุ๋มแล้สแวดงผลกาทำงาน
+//  //MaterialButton with Onpressed use couple
+//
+//
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return Column(
+//      children: <Widget>[
+//        Image.asset('assets/images/pic1.jpg'),
+//        MaterialButton(
+//          child: Text("Show details",style: TextStyle(color: Colors.amber)),
+//          onPressed: () => setState(() {
+//            opacity = 1;
+//          }),
+//        ),
+//        AnimatedOpacity(
+//          duration: Duration(seconds: 2),
+//          child: Column(
+//            children: <Widget>[
+//              Text("Type: Owl"),
+//              Text("Food Small mammals, insect")
+//            ],
+//          ),
+//          opacity: opacity,
+//        )
+//      ],
+//    );
+//  }
+//
+//}
+
+//ShapeShiht
+
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+double randomBorderRadius() {
+  return Random().nextDouble() * 64;
+}
+
+double randomMargin() {
+  return Random().nextDouble() * 64;
+}
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-   return MaterialApp(
-     title: "Animation",
-     home: Scaffold(
-       appBar: AppBar(title: Text("Animation"),),
-       body: FadeInDemo(),
-       //Show FadeInDemo Class
-     ),
-
-   );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Test Animation",
+        home: AnimatedContainerDemo());
   }
 }
 
-// FadeInDemo class create FadeInDemoState and extends StatefulWidget
-class FadeInDemo extends StatefulWidget{
-  FadeInDemoState createState() => FadeInDemoState();
-  //FadeInDemoState createState () => FadeInDemoState();
+class AnimatedContainerDemo extends StatefulWidget {
+  AnimatedContainerDemoState createState() => AnimatedContainerDemoState();
 }
 
-class FadeInDemoState extends State<FadeInDemo>{
-  var opacity = 0.0;
+class AnimatedContainerDemoState extends State<AnimatedContainerDemo> {
+  var borderRadius = randomBorderRadius();
+  var margin = randomMargin();
+
+  void change() {
+    setState(() {
+      borderRadius = randomBorderRadius();
+      margin = randomMargin();
+    });
+  }
+
   @override
-
-   //Display Main State
-  // onPressed สำหนับกดปุ๋มแล้สแวดงผลกาทำงาน
-  //MaterialButton with Onpressed use couple
-
-
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Column(
-      children: <Widget>[
-        Image.asset('assets/images/pic1.jpg'),
-        MaterialButton(
-          child: Text("Show details",style: TextStyle(color: Colors.amber)),
-          onPressed: () => setState(() {
-            opacity = 1;
-          }),
+    return Scaffold(
+      appBar: AppBar(title: Text("Animation")),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              width: 128,
+              height: 128,
+              child: AnimatedContainer(
+                duration: Duration(seconds: 2),
+                margin: EdgeInsets.all(margin),
+                decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(borderRadius)),
+              ),
+            ),
+            MaterialButton(
+                color: Colors.blue,
+                child: Text(
+                  "Change",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () => change())
+          ],
         ),
-        AnimatedOpacity(
-          duration: Duration(seconds: 2),
-          child: Column(
-            children: <Widget>[
-              Text("Type: Owl"),
-              Text("Food Small mammals, insect")
-            ],
-          ),
-          opacity: opacity,
-        )
-      ],
+      ),
     );
   }
-
-
-
 }
